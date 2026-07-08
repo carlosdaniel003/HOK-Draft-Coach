c069baa51cdd1970bd2f96a1a30094d12a2e0927
Carlos Daniel <160197491+carlosdaniel003@users.noreply.github.com>
2026-07-08 15:56:36 -0400

    feat: expose role inference endpoint

--- src/main/java/br/com/carlosdaniel/hokdraftcoach/controller/DraftController.java
+++ src/main/java/br/com/carlosdaniel/hokdraftcoach/controller/DraftController.java
@@ -7,7 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.carlosdaniel.hokdraftcoach.dto.AnaliseDraftResponse;
 import br.com.carlosdaniel.hokdraftcoach.dto.DraftRequest;
+import br.com.carlosdaniel.hokdraftcoach.dto.InferenciaFuncoesRequest;
+import br.com.carlosdaniel.hokdraftcoach.dto.InferenciaFuncoesResponse;
 import br.com.carlosdaniel.hokdraftcoach.service.DraftService;
+import br.com.carlosdaniel.hokdraftcoach.service.InferenciaFuncoesService;
 import jakarta.validation.Valid;
 
 @RestController
@@ -15,9 +18,14 @@ import jakarta.validation.Valid;
 public class DraftController {
 
     private final DraftService draftService;
+    private final InferenciaFuncoesService inferenciaFuncoesService;
 
-    public DraftController(DraftService draftService) {
+    public DraftController(
+        DraftService draftService,
+        InferenciaFuncoesService inferenciaFuncoesService
+    ) {
         this.draftService = draftService;
+        this.inferenciaFuncoesService = inferenciaFuncoesService;
     }
 
     @PostMapping("/recomendar")
@@ -25,5 +33,12 @@ public class DraftController {
         @Valid @RequestBody DraftRequest request
     ) {
         return draftService.recomendar(request);
+    }
+
+    @PostMapping("/inferir-funcoes")
+    public InferenciaFuncoesResponse inferirFuncoes(
+        @Valid @RequestBody InferenciaFuncoesRequest request
+    ) {
+        return inferenciaFuncoesService.inferir(request);
     }
 }
