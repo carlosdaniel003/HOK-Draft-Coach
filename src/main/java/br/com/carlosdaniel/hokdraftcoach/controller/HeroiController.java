c5389d6c45161dc5bfac29328757be06913265d7
Carlos Daniel <160197491+carlosdaniel003@users.noreply.github.com>
2026-07-08 14:38:17 -0400

    feat: allow filtering heroes by role

--- src/main/java/br/com/carlosdaniel/hokdraftcoach/controller/HeroiController.java
+++ src/main/java/br/com/carlosdaniel/hokdraftcoach/controller/HeroiController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.carlosdaniel.hokdraftcoach.model.Heroi;
+import br.com.carlosdaniel.hokdraftcoach.model.Rota;
 import br.com.carlosdaniel.hokdraftcoach.service.HeroiService;
 
 @RestController
@@ -20,7 +22,13 @@ public class HeroiController {
     }
 
     @GetMapping
-    public List<Heroi> listarHerois() {
-        return heroiService.listarTodos();
+    public List<Heroi> listarHerois(
+        @RequestParam(required = false) Rota rota
+    ) {
+        if (rota == null) {
+            return heroiService.listarTodos();
+        }
+
+        return heroiService.listarPorRota(rota);
     }
 }
