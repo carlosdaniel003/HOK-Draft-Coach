33fc960ef2a19b7c213475978cb80c17ce5f5a0a
Carlos Daniel <160197491+carlosdaniel003@users.noreply.github.com>
2026-07-09 08:43:49 -0400

    feat: add mid-lane archetypes

---
+++ src/main/java/br/com/carlosdaniel/hokdraftcoach/model/ArquetipoMid.java
@@ -0,0 +1,19 @@
+package br.com.carlosdaniel.hokdraftcoach.model;
+
+public enum ArquetipoMid {
+    ARTILHEIRO,
+    CONTROLADOR,
+    MAGO_DE_BATALHA,
+    ASSASSINO_MAGICO,
+    EXPLOSAO,
+    POKE,
+    ZONEAMENTO,
+    PICKOFF,
+    CERCO,
+    PUSHER,
+    ROAMER,
+    ANTI_DIVE,
+    SUSTENTACAO,
+    ESCALAMENTO,
+    UTILITARIO
+}
