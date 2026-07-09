c424889611330bfc1e4701fd8bcd843662b942d4
Carlos Daniel <160197491+carlosdaniel003@users.noreply.github.com>
2026-07-09 13:27:13 -0400

    feat: define composition win condition types

---
+++ src/main/java/br/com/carlosdaniel/hokdraftcoach/model/CondicaoVitoriaTipo.java
@@ -0,0 +1,13 @@
+package br.com.carlosdaniel.hokdraftcoach.model;
+
+public enum CondicaoVitoriaTipo {
+    PROTEGER_HIPERCARREGADOR,
+    SPLIT_PUSH,
+    CERCO_DE_TORRES,
+    LUTAS_LONGAS,
+    PICKOFF,
+    DIVE_NA_RETAGUARDA,
+    WOMBO_COMBO,
+    CONTROLE_DE_OBJETIVOS,
+    ESCALAMENTO_TARDIO
+}
