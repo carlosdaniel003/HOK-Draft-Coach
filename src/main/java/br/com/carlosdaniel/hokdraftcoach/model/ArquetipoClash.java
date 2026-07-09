f100c39314977362b7eddacb31bfac3a664a08cd
Carlos Daniel <160197491+carlosdaniel003@users.noreply.github.com>
2026-07-09 10:28:01 -0400

    feat: add clash lane archetypes

---
+++ src/main/java/br/com/carlosdaniel/hokdraftcoach/model/ArquetipoClash.java
@@ -0,0 +1,19 @@
+package br.com.carlosdaniel.hokdraftcoach.model;
+
+public enum ArquetipoClash {
+    DUELISTA,
+    BRUISER,
+    TANQUE,
+    JUGGERNAUT,
+    SPLIT_PUSHER,
+    INICIADOR,
+    FLANQUEADOR,
+    MAGO_DE_ROTA,
+    ANTI_TANQUE,
+    ANTI_CARRY,
+    SUSTENTACAO,
+    ESCALAMENTO,
+    CONTROLADOR,
+    GLOBAL,
+    UTILITARIO
+}
