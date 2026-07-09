ee3a72274e9c29908aae6150be87f56ddc2a9578
Carlos Daniel <160197491+carlosdaniel003@users.noreply.github.com>
2026-07-09 13:57:43 -0400

    feat: define multi-hero synergy types

---
+++ src/main/java/br/com/carlosdaniel/hokdraftcoach/model/TipoSinergiaGrupo.java
@@ -0,0 +1,11 @@
+package br.com.carlosdaniel.hokdraftcoach.model;
+
+public enum TipoSinergiaGrupo {
+    CONTROLE_EM_CADEIA,
+    COMBO_DE_ULTIMATES,
+    SEQUENCIA_DE_ENGAGE,
+    PROTECAO_EM_CAMADAS,
+    RESET_DE_HABILIDADES,
+    AGRUPAMENTO_SEGUIDO_DE_DANO_EM_AREA,
+    WOMBO_COMBO
+}
