f46e4fb9007ad793d18ae9ab68cd8018a8028fca
Carlos Daniel <160197491+carlosdaniel003@users.noreply.github.com>
2026-07-09 10:28:11 -0400

    feat: classify clash lane matchup advantages

---
+++ src/main/java/br/com/carlosdaniel/hokdraftcoach/model/TipoVantagemClash.java
@@ -0,0 +1,13 @@
+package br.com.carlosdaniel.hokdraftcoach.model;
+
+public enum TipoVantagemClash {
+    ROTA,
+    DUELO,
+    SUSTENTACAO,
+    MECANICA,
+    WAVE_CLEAR,
+    SPLIT_PUSH,
+    ROTACAO,
+    LUTA,
+    ESCALAMENTO
+}
