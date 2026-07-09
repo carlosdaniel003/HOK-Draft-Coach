57a97c795687138b6df6ef7f0c6c42f0c9c64dce
Carlos Daniel <160197491+carlosdaniel003@users.noreply.github.com>
2026-07-09 07:27:37 -0400

    feat: add composition archetypes for support analysis

---
+++ src/main/java/br/com/carlosdaniel/hokdraftcoach/model/TipoComposicao.java
@@ -0,0 +1,19 @@
+package br.com.carlosdaniel.hokdraftcoach.model;
+
+public enum TipoComposicao {
+    DIVE,
+    ENGAGE_AGRUPADO,
+    POKE,
+    EXPLOSAO,
+    LUTAS_LONGAS,
+    FRONT_TO_BACK,
+    HIPERCARREGADOR,
+    ESCUDOS_E_CURA,
+    ALVOS_IMOVEIS,
+    ALTA_MOBILIDADE,
+    CONTROLE_PESADO,
+    SPLIT_PUSH,
+    ULTIMATES_DECISIVAS,
+    PICKOFF,
+    DESENGAGE
+}
