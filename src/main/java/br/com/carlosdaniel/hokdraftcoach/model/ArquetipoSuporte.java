c6addb742443ba7ad188fa9ef505d7645e638a21
Carlos Daniel <160197491+carlosdaniel003@users.noreply.github.com>
2026-07-09 07:26:54 -0400

    feat: add support archetype model

---
+++ src/main/java/br/com/carlosdaniel/hokdraftcoach/model/ArquetipoSuporte.java
@@ -0,0 +1,16 @@
+package br.com.carlosdaniel.hokdraftcoach.model;
+
+public enum ArquetipoSuporte {
+    INICIADOR,
+    PROTETOR,
+    CURADOR,
+    ENCANTADOR,
+    PICKOFF,
+    DESENGAGE,
+    MACRO,
+    POKE,
+    AMPLIFICADOR,
+    ANTI_CONTROLE,
+    TANQUE,
+    CONTROLE_DE_AREA
+}
